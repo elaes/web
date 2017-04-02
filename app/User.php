6116b7d62ac9a461035d39baf5538e62ea42e9de
elaes <coconutonut@msn.com>
2017-04-02 08:50:00 +0200

    ajout User et Post dans la base

--- app/User.php
+++ app/User.php
@@ -9,21 +9,17 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $fillable = ['name', 'email', 'password','admin',];
+    
+    protected $hidden = ['password', 'remember_token',];
+    
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+    
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
